fac326c5baf7c583f210ed5590e7ae073d5616d4
Alejandro <alejandro.bmslatinoamerica@gmail.com>
2025-11-06 16:53:47 -0500

    Primer Commit

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,13 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: 'login',
+    loadComponent: () =>
+      import('./poli-encuestas/pages/login-page/login-page.component'),
+  },
+  {
+    path: '**',
+    redirectTo: 'login'
+  }
+];
