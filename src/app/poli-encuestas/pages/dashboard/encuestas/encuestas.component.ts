c83a387ce0221c44b250ed607055cfa30a15602f
Alejandro Cortes <acr20020221@gmail.com>
2025-11-17 21:00:06 -0500

    se hace la integracion surveyts

--- src/app/poli-encuestas/pages/dashboard/encuestas/encuestas.component.ts
+++ src/app/poli-encuestas/pages/dashboard/encuestas/encuestas.component.ts
@@ -1,11 +1,48 @@
 import { Component } from '@angular/core';
+import { SurveyModule } from "survey-angular-ui";
+import { Model } from 'survey-core';
+// survey.component.ts
+import "survey-core/survey-core.min.css";
+
+
+const surveyJson = {
+  pages: [{
+    name: "PersonalDetails",
+    elements: [{
+      type: "text",
+      name: "FirstName",
+      title: "Enter your first name:"
+    }, {
+      type: "text",
+      name: "LastName",
+      title: "Enter your last name:"
+    }, {
+      type: "panel",
+      name: "Contacts",
+      state: "collapsed",
+      title: "Contacts (optional)",
+      elements: [{
+        type: "text",
+        name: "Telegram",
+        title: "Telegram:"
+      }, {
+        type: "text",
+        name: "GitHub",
+        title: "GitHub username:"
+      }]
+    }]
+  }]
+};
 
 @Component({
   selector: 'app-encuestas',
-  imports: [],
+  imports: [SurveyModule],
   templateUrl: './encuestas.component.html',
   styleUrl: './encuestas.component.css'
 })
 export class EncuestasComponent {
 
+  surveyModel: Model = new Model(surveyJson);
+
+  constructor() { }
 }
